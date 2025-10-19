eacff0f0f6dd201fce50678e4989ff4bbcfa8c8a
Havilash Sivaratnam <havilash.sivaratnam@gmail.com>
2025-10-19 16:00:43 +0200

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,5 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -10,40 +9,15 @@ if true then return {} end
 -- * override the configuration of LazyVim plugins
 return {
   {
-    "shaunsingh/nord.nvim",
-    config = function()
-      vim.g.nord_contrast = true
-      vim.g.nord_borders = false
-      vim.g.nord_disable_background = true
-      vim.g.nord_italic = true
-      vim.g.nord_uniform_diff_background = true
-      vim.g.nord_bold = false
-
-      vim.opt.background = "dark"
-    end,
-  },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
+    "folke/tokyonight.nvim",
+    lazy = true,
     opts = {
-      colorscheme = "nord",
-    },
-  },
-
-  -- add pyright to lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
-      },
+      style = "moon",
+      transparent = true,
     },
   },
 
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.vue" },
 }
